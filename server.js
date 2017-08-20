5064656e56e9a8f758af8497a2cff323a11fa258
dandan53 <dandan53@users.noreply.github.com>
2017-08-20 18:26:05 +0300

    First commit

--- server.js
+++ server.js
@@ -1,44 +1,20 @@
 var http = require('http');
 
-var fs = require("fs");
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.write('<!DOCTYPE html>'+
+'<html>'+
+' <head>'+
+' <meta charset="utf-8" />'+
+' <title>My Node.js page!</title>'+
+' </head>'+ 
+' <body>'+
+' <p>Here is a paragraph of <strong>HTML!!</strong>!</p>'+
+' </body>'+
+'</html>');
+res.end();
+});
 
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-http.createServer(function(request, response) {
-
-	if(request.url === "/"){
-		console.log("call");
-		sendFileContent(response, "index.html", "text/html");
-	}
-	else if(request.url === "/index"){
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<b>Hey there!</b><br /><br />Hi, This is the default response. Requested URL is: ' + request.url);
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else{
-		console.log("Requested URL is: " + request.url);
-		response.end();
-	}
-}).listen(server_port);
-
-function sendFileContent(response, fileName, contentType){
-	fs.readFile(fileName, function(err, data){
-		if(err){
-			response.writeHead(404);
-			response.write("Not Found!");
-		}
-		else{
-			response.writeHead(200, {'Content-Type': contentType});
-			response.write(data);
-		}
-		response.end();
-	});
-}
-
+server.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
