b298f7abdaf09ca7fba6065e76440eb1f330e020
dandan53 <dandan53@users.noreply.github.com>
2017-08-28 13:40:02 +0300

    dd

---
+++ server1.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function(request, response){
+    console.warn(request);
+    var date = new Date();
+    var dateString = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<b>Hello World</b><br /><i>The date is: ' + dateString + '</i><br /><br />The requested URL is: ' + request.url);    
+}).listen(8080);
