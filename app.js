b2b7c3a5e7f8da4b1f4b81238a697b837931f45e
dandan53 <dandan53@users.noreply.github.com>
2017-08-28 22:54:57 +0300

    ff

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-
-// Define the port to run on
-app.set('port', 8080);
-
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public'));
-//app.use('/public', express.static(path.join(__dirname + '/public')));
-
-app.get('/a', function(req, res){
-  console.log('hub.challenge11111');
-res.send(__dirname);
-  //res.send('hub.challenge');
-   // res.send(req.query['hub.challenge']);
-});
-
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(8080, () => console.log('Server running on port 3000'))
